a557e82fb990a729ded457c3b42c7f1b65565253
Longjiang Lv <lannik1107@gmail.com>
2015-09-29 16:54:58 +0800

    Disable bitcode in pod packages.

--- Source/SSPhoto.swift
+++ Source/SSPhoto.swift
@@ -218,7 +218,7 @@ extension SSPhoto {
 // MARK: - Async Loading
 extension SSPhoto {
     func decodedImageWithImage(image:UIImage) -> UIImage? {
-        if let gif = image.images {
+        if let _ = image.images {
             return image
         }
         
