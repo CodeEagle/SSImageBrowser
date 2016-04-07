8ad91bfe01540e24ddb97f7f50dff1a732984dc8
CodeEagle <stasura@hotmail.com>
2016-04-07 16:54:23 +0800

    fixed bug
    #6

--- Source/SSCaptionView.swift
+++ Source/SSCaptionView.swift
@@ -9,126 +9,126 @@
 import UIKit
 
 public class SSCaptionView: UIView {
-    var photo: SSPhoto!
-    
-    private var labelPadding: CGFloat {
-        return 10
-    }
-    private var label: UILabel!
-    
-    convenience init(aPhoto:SSPhoto) {
-        let screenBound = UIScreen.mainScreen().bounds
-        var screenWidth = screenBound.size.width
-        let orientation = UIDevice.currentDevice().orientation
-        if orientation == .LandscapeLeft ||
-            orientation == .LandscapeRight {
-                screenWidth = screenBound.size.height
-        }
-        self.init(frame: CGRectMake(0, 0, screenWidth, 44))
-        photo = aPhoto
-        opaque = false
-        setBackground()
-        setupCaption()
-    }
-    
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
+	var photo: SSPhoto!
 
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-        if let view = viewWithTag(101) {
-            view.frame = CGRectMake(0, -100, UIScreen.mainScreen().bounds.width, 130+100)
-        }
-        
-    }
-    /**
-    To create your own custom caption view, subclass this view
-    and override the following two methods (as well as any other
-    UIView methods that you see fit):
-    
-    Override -setupCaption so setup your subviews and customise the appearance
-    of your custom caption
-    You can access the photo's data by accessing the _photo ivar
-    If you need more data per photo then simply subclass IDMPhoto and return your
-    subclass to the photo browsers -photoBrowser:photoAtIndex: delegate method
-    
-    */
-    public func setupCaption() {
-        
-        label                  = UILabel(frame: CGRectMake(labelPadding, 0, self.bounds.size.width-labelPadding*2, self.bounds.size.height))
-        label.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        label.opaque           = false
-        label.backgroundColor  = UIColor.clearColor()
-        label.textAlignment    = .Center
-        label.lineBreakMode    = NSLineBreakMode.ByWordWrapping
-        label.numberOfLines    = 3
-        label.textColor        = UIColor.whiteColor()
-        label.shadowColor      = UIColor(white: 0, alpha: 0.5)
-        label.shadowOffset     = CGSizeMake(0, 1)
-        label.font             = UIFont.systemFontOfSize(17)
-        label.text             = ""
-        if let cap = photo?.caption() {
-            label.text = cap
-        }
-        addSubview(label)
-    }
-    
-    /**
-    Override -sizeThatFits: and return a CGSize specifying the height of your
-    custom caption view. With width property is ignored and the caption is displayed
-    the full width of the screen
+	private var labelPadding: CGFloat {
+		return 10
+	}
+	private var label: UILabel!
 
-    
-    :param: size CGSize
-    
-    :returns: CGSize
-    */
-    public override func sizeThatFits(size: CGSize) -> CGSize {
-        if label.text == nil  {
-            return CGSizeZero
-        }
-        
-        if let b = label.text?.isEmpty  {
-            if b {
-                return CGSizeZero
-            }
-        }
-        
-        var maxHeight: CGFloat = 9999
-        if label.numberOfLines > 0  {
-            maxHeight = label.font.leading * CGFloat(label.numberOfLines)
-        }
-        
-        
-        let text           = label.text!
-        let width          = size.width - labelPadding*2
-        let font           = label.font
+	convenience init(aPhoto: SSPhoto) {
+		let screenBound = UIScreen.mainScreen().bounds
+		var screenWidth = screenBound.size.width
+		let orientation = UIDevice.currentDevice().orientation
+		if orientation == .LandscapeLeft ||
+		orientation == .LandscapeRight {
+			screenWidth = screenBound.size.height
+		}
+		self.init(frame: CGRectMake(0, 0, screenWidth, 44))
+		photo = aPhoto
+		opaque = false
+		setBackground()
+		setupCaption()
+	}
 
-        let attributedText = NSAttributedString(string: text, attributes: [NSFontAttributeName: font])
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
 
-        let rect           = attributedText.boundingRectWithSize(CGSizeMake(width, maxHeight), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
 
-        let textSize       = rect.size
-        
-        return CGSizeMake(size.width, textSize.height + labelPadding * 2)
-    }
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		if let view = viewWithTag(101) {
+			let len = max(UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
+			view.frame = CGRectMake(0, -100, len, 130 + 100)
+		}
+	}
+	/**
+	 To create your own custom caption view, subclass this view
+	 and override the following two methods (as well as any other
+	 UIView methods that you see fit):
+
+	 Override -setupCaption so setup your subviews and customise the appearance
+	 of your custom caption
+	 You can access the photo's data by accessing the _photo ivar
+	 If you need more data per photo then simply subclass IDMPhoto and return your
+	 subclass to the photo browsers -photoBrowser:photoAtIndex: delegate method
+
+	 */
+	public func setupCaption() {
+
+		label = UILabel(frame: CGRectMake(labelPadding, 0, self.bounds.size.width - labelPadding * 2, self.bounds.size.height))
+		label.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+		label.opaque = false
+		label.backgroundColor = UIColor.clearColor()
+		label.textAlignment = .Center
+		label.lineBreakMode = NSLineBreakMode.ByWordWrapping
+		label.numberOfLines = 3
+		label.textColor = UIColor.whiteColor()
+		label.shadowColor = UIColor(white: 0, alpha: 0.5)
+		label.shadowOffset = CGSizeMake(0, 1)
+		label.font = UIFont.systemFontOfSize(17)
+		label.text = ""
+		if let cap = photo?.caption() {
+			label.text = cap
+		}
+		addSubview(label)
+	}
+
+	/**
+	 Override -sizeThatFits: and return a CGSize specifying the height of your
+	 custom caption view. With width property is ignored and the caption is displayed
+	 the full width of the screen
+
+
+	 :param: size CGSize
+
+	 :returns: CGSize
+	 */
+	public override func sizeThatFits(size: CGSize) -> CGSize {
+		if label.text == nil {
+			return CGSizeZero
+		}
+
+		if let b = label.text?.isEmpty {
+			if b {
+				return CGSizeZero
+			}
+		}
+
+		var maxHeight: CGFloat = 9999
+		if label.numberOfLines > 0 {
+			maxHeight = label.font.leading * CGFloat(label.numberOfLines)
+		}
+
+		let text = label.text!
+		let width = size.width - labelPadding * 2
+		let font = label.font
+
+		let attributedText = NSAttributedString(string: text, attributes: [NSFontAttributeName: font])
+
+		let rect = attributedText.boundingRectWithSize(CGSizeMake(width, maxHeight), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+
+		let textSize = rect.size
+
+		return CGSizeMake(size.width, textSize.height + labelPadding * 2)
+	}
 }
 
 extension SSCaptionView {
-    func setBackground() {
-        let fadeView = UIView(frame:CGRectMake(0, -100, UIScreen.mainScreen().bounds.width, 130+100)) // Static width, autoresizingMask is not working
-        fadeView.tag = 101
-        let gradient = CAGradientLayer()
-        gradient.frame = fadeView.bounds
-        gradient.colors = [UIColor(white: 0, alpha: 0).CGColor,UIColor(white: 0, alpha: 0.8).CGColor]
-        fadeView.layer.insertSublayer(gradient, atIndex: 0)
-        fadeView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        addSubview(fadeView)
-    }
-    
+
+	func setBackground() {
+		let len = max(UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
+		let fadeView = UIView(frame: CGRectMake(0, -100, len, 130 + 100)) // Static width, autoresizingMask is not working
+		fadeView.tag = 101
+		let gradient = CAGradientLayer()
+		gradient.frame = fadeView.bounds
+		gradient.colors = [UIColor(white: 0, alpha: 0).CGColor, UIColor(white: 0, alpha: 0.8).CGColor]
+		fadeView.layer.insertSublayer(gradient, atIndex: 0)
+		fadeView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+		addSubview(fadeView)
+	}
 }
