6640799ebbbaa7553af44f1dbb778db468c49d94
CodeEagle <stasura@hotmail.com>
2016-04-25 11:21:27 +0800

    fixed bug
    #8 thanks a lot

--- Source/SSTapDetectingImageView.swift
+++ Source/SSTapDetectingImageView.swift
@@ -8,61 +8,58 @@
 
 import UIKit
 public protocol SSTapDetectingImageViewDelegate: class {
-    func imageView(imageView: UIImageView, singleTapDetected touch: UITouch)
-    func imageView(imageView: UIImageView, doubleTapDetected touch: UITouch)
-    func imageView(imageView: UIImageView, tripleTapDetected touch: UITouch)
+	func imageView(imageView: UIImageView, singleTapDetected touch: UITouch)
+	func imageView(imageView: UIImageView, doubleTapDetected touch: UITouch)
+	func imageView(imageView: UIImageView, tripleTapDetected touch: UITouch)
 }
 public class SSTapDetectingImageView: UIImageView {
-    public var tapDelegate: SSTapDetectingImageViewDelegate!
-    
-    
-    override init(image: UIImage!) {
-        super.init(image: image)
-        initialize()
-    }
-    
-    override init(image: UIImage!, highlightedImage: UIImage?) {
-        super.init(image: image, highlightedImage: highlightedImage)
-        initialize()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initialize()
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        initialize()
-    }
-    
-    func initialize() {
-        self.userInteractionEnabled = true
-    }
-    
+	public weak var tapDelegate: SSTapDetectingImageViewDelegate!
+
+	override init(image: UIImage!) {
+		super.init(image: image)
+		initialize()
+	}
+
+	override init(image: UIImage!, highlightedImage: UIImage?) {
+		super.init(image: image, highlightedImage: highlightedImage)
+		initialize()
+	}
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		initialize()
+	}
+
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		initialize()
+	}
+
+	func initialize() {
+		self.userInteractionEnabled = true
+	}
 }
 extension SSTapDetectingImageView {
-    
-    public override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
 
-        let tapCount = touch.tapCount
-        switch tapCount {
-        case 1:
-            tapDelegate?.imageView(self, singleTapDetected: touch)
-            break
-        case 2:
-            tapDelegate?.imageView(self, doubleTapDetected: touch)
-            break
-        case 3:
-            tapDelegate?.imageView(self, tripleTapDetected: touch)
-            break
-        default:
-            break
-        }
-        self.nextResponder()?.touchesEnded(touches, withEvent: event)
+	public override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		guard let touch = touches.first else {
+			return
+		}
 
-    }
+		let tapCount = touch.tapCount
+		switch tapCount {
+		case 1:
+			tapDelegate?.imageView(self, singleTapDetected: touch)
+			break
+		case 2:
+			tapDelegate?.imageView(self, doubleTapDetected: touch)
+			break
+		case 3:
+			tapDelegate?.imageView(self, tripleTapDetected: touch)
+			break
+		default:
+			break
+		}
+		self.nextResponder()?.touchesEnded(touches, withEvent: event)
+	}
 }
